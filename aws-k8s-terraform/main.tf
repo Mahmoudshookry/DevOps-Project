6b0cd24d3f9389e5fff7d05ed644cb9ff3738789
Mohamed Mohamed Abd El Kawy <126289262+m3bdlkawy@users.noreply.github.com>
2024-10-15 21:38:59 +0300

    Update main.tf

--- aws-k8s-terraform/main.tf
+++ aws-k8s-terraform/main.tf
@@ -6,13 +6,13 @@ terraform {
     encrypt        = false
   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "= 5.71.0"
-    }
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 5.0"
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"
