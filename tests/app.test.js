8e4b7bc047ad5168daa0aa0bd4ace0eed1ff150e
Mohamed Mohamed Abd El Kawy <126289262+m3bdlkawy@users.noreply.github.com>
2024-10-15 13:31:53 +0300

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,12 @@
 const request = require('supertest');
-const app = require('../app'); // Adjust path as needed
+const app = require('../app'); // Adjust the path to your app file
 
-describe('Test the root path', () => {
-    it('should respond with a 200 status code', async () => {
-        const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-    });
+describe('GET /', () => {
+  it('should return status code 200 and contain teammates list', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toContain('Hello, Teammates!');
+    expect(response.text).toContain('mohamed abdelfattah maghawry mostafa');
+    expect(response.text).toContain('Mahmoud Mohamed Ali Shokry');
+  });
 });
